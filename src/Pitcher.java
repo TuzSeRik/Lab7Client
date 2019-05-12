e98e7aa3d01dcbeadb9b4a68eee444c8fa544aa9
Joker <jokerbladeofstorme@gmail.com>
2019-05-13 01:05:02 +0300

    Client ver 0.1.0

---
+++ src/Pitcher.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+class Pitcher {
+    private Socket socket;
+
+    Pitcher(Socket socket) {
+        this.socket = socket;
+    }
+
+
+
+    void pitch(InputStream command) throws IOException {
+        socket.getOutputStream().write(command.read());
+    }
+
+    InputStream complexPitch(InputStream command) throws IOException{
+        socket.getOutputStream().write(command.read());
+        return socket.getInputStream();
+    }
+}
+//+
